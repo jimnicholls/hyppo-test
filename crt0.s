05bec332ff5f01d3201329710d22b1a435e6e081
Jim Nicholls <jim.nicholls@gmail.com>
2021-12-11 21:24:04 +1100

    Initialise the cc65 runtime.

--- crt0.s
+++ crt0.s
@@ -3,9 +3,15 @@
 .export         __STARTUP__: absolute = 1
 .export         _exit
 
+.import         __MAIN_START__, __MAIN_SIZE__, __STACKSIZE__
+.import         _clrscr, _conioinit
 .import         _main
-.import         __MAIN_START__, __MAIN_SIZE__           ; Linker generated
-.import         __STACKSIZE__                           ; … from the configure file
+.import         callmain, donelib, initlib, zerobss
+
+.pushcpu
+.setcpu         "65C02"                                 ; Avoid SP being a keyword
+.include        "cc65/asminc/zeropage.inc"
+.popcpu
 
 
 ; System variables
@@ -36,15 +42,15 @@ border_col      := vic_base + $20
 
 
 .segment        "STARTUP"
-
 .proc           startup
         sei
         jsr     init
-        jsr     _main
-.endproc
-
-.proc           _exit
-        sei
+        cli
+        jsr     zerobss                                 ; Cannot call any code in the ONCE segment from this point
+        jsr     callmain
+.endproc                                                ; Falls through to _exit
+.proc           _exit                                   ; Must immediately follow startup
+        jsr     done
         lda     #0
 @10:    sta     border_col
         inc
@@ -53,19 +59,38 @@ border_col      := vic_base + $20
 
 
 .segment        "ONCE"
-
 .proc           init
-        jsr     init_irqhandler
+        lda     #1                                      ; Don't call BASIC in the kernel's IRQ handler
+        trb     init_status
         jsr     init_memory_map
+        jsr     init_cc65
+        jsr     _conioinit
+       ;jsr     _clrscr
         rts
 .endproc
 
-.proc           init_irqhandler: near
-        lda     #1                                      ; Don't call BASIC in the kernel IRQ handler
-        trb     init_status
+
+.segment        "ONCE"
+.proc           init_cc65
+.pushcpu
+.setcpu         "65C02"                                 ; Avoid SP being a keyword
+        ldx     #zpspace - 1                            ; Save zero-page locations
+@10:    lda     sp, x
+        sta     zp_save, x
+        dex
+        bpl     @10
+        lda     #<(__MAIN_START__ + __MAIN_SIZE__)      ; Set up the stack
+        ldx     #>(__MAIN_START__ + __MAIN_SIZE__)
+        sta     sp
+        stx     sp+1
+        jsr     initlib                                 ; Clears the I flag
+        rts
+.popcpu
 .endproc
 
-.proc           init_memory_map: near
+
+.segment        "ONCE"
+.proc           init_memory_map
         lda     #5                                      ; Map ROM into $C000-$CFFF
         tsb     $d030
         lda     #%101                                   ; … I/O into $D000—$DFFF
@@ -74,8 +99,26 @@ border_col      := vic_base + $20
         tax
         tay
         ldz     #%10000011                              ; … and kernel into $E000—$FFFF
+        jsr     set_environment
         map
         eom
-        jsr     set_environment
         rts
 .endproc
+
+
+.segment        "CODE"
+.proc           done
+.pushcpu
+.setcpu         "65C02"                                 ; Avoid SP being a keyword
+        jsr     donelib
+        ldx     #zpspace - 1                            ; Restore zero-page locations
+@10:    lda     zp_save, x
+        sta     sp, x
+        dex
+        bpl     @10
+.popcpu
+.endproc
+
+
+.segment        "INIT"
+zp_save:        .res zpspace
